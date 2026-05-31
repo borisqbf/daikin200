a9b0e27e4cf41b894802cc2c6e8bce2f394d2680
borisqbf <borisqbf@users.noreply.github.com>
2026-05-31 22:54:38 +1000

    formatting

--- components/daikin200/climate.h
+++ components/daikin200/climate.h
@@ -3,19 +3,22 @@
 #include "esphome/components/climate/climate.h"
 #include "esphome/core/component.h"
 
-namespace esphome {
-namespace daikin200 {
+namespace esphome
+{
+  namespace daikin200
+  {
 
-class Daikin200Climate : public climate::Climate, public Component {
- public:
-  void setup() override;
-  void control(const climate::ClimateCall &call) override;
-  climate::ClimateTraits traits() override;
+    class Daikin200Climate : public climate::Climate, public Component
+    {
+    public:
+      void setup() override;
+      void control(const climate::ClimateCall &call) override;
+      climate::ClimateTraits traits() override;
 
- protected:
-  void build_frame();
-  void send_frame();
-  uint8_t frame[25]{0};  // your Daikin200 payload buffer
-};
-}  // namespace daikin200
-}  // namespace esphome
+    protected:
+      void build_frame();
+      void send_frame();
+      uint8_t frame[25]{0}; // your Daikin200 payload buffer
+    };
+  } // namespace daikin200
+} // namespace esphome
