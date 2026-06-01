bb36d9d720c08bdba987e0421403fa10819d767b
borisqbf <borisqbf@users.noreply.github.com>
2026-06-01 19:42:19 +1000

    add diagnostic

--- components/daikin200/climate.py
+++ components/daikin200/climate.py
@@ -1,6 +1,10 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import climate
+
+print("CLIMATE ATTRS:")
+print([x for x in dir(climate) if "SCHEMA" in x])
+
 from esphome.const import CONF_ID, CONF_NAME
 
 from . import Daikin200Climate
