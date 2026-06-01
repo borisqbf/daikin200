e1372bb83aef3dbbe79c4facd647f787b4e53b86
borisqbf <borisqbf@users.noreply.github.com>
2026-06-01 20:05:34 +1000

    removed CONF_REMOTE_TRANSMITTER

--- components/daikin200/climate.py
+++ components/daikin200/climate.py
@@ -3,19 +3,21 @@ import esphome.config_validation as cv
 from esphome.components import climate
 from esphome.components import remote_base
 
-from esphome.const import CONF_ID, CONF_NAME, CONF_REMOTE_TRANSMITTER
+from esphome.const import CONF_ID, CONF_NAME
 
 
 from . import Daikin200Climate
 
 CONFIG_SCHEMA = climate._CLIMATE_SCHEMA.extend({
     cv.GenerateID(): cv.declare_id(Daikin200Climate),
-    cv.Required(CONF_REMOTE_TRANSMITTER): cv.use_id(remote_base.RemoteTransmitterComponent),
+
+    cv.Required("remote_transmitter"): cv.use_id(remote_base.RemoteTransmitterComponent),
 }).extend(cv.COMPONENT_SCHEMA)
 
 def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
-    tx = yield cg.get_variable(config[CONF_REMOTE_TRANSMITTER])
+    tx = yield cg.get_variable(config["remote_transmitter"])
+
     cg.add(var.set_transmitter(tx))
 
     yield cg.register_component(var, config)
