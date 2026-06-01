18685935bea0c95f66496984b980f47af4d05509
borisqbf <borisqbf@users.noreply.github.com>
2026-06-01 19:08:40 +1000

    aand one more

--- components/daikin200/climate.py
+++ components/daikin200/climate.py
@@ -2,17 +2,17 @@ import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import climate
 from esphome.const import CONF_ID
-
+from esphome.const import CONF_ID, CONF_NAME
 from . import Daikin200Climate
+
 
 CONFIG_SCHEMA = cv.Schema({
     cv.GenerateID(): cv.declare_id(Daikin200Climate),
+    cv.Optional(CONF_NAME): cv.string,
 }).extend(cv.COMPONENT_SCHEMA)
-
 
 def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     cg.add(var)
     yield cg.register_component(var, config)
     yield climate.register_climate(var, config)
-    
