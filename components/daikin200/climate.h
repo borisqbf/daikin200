a27c6ff64896cacdf72c37174a71d570b70bb4e8
borisqbf <borisqbf@users.noreply.github.com>
2026-06-01 22:54:48 +1000

    use IRremoteESP8266

--- components/daikin200/climate.h
+++ components/daikin200/climate.h
@@ -1,42 +1,33 @@
 #pragma once
 
-#include "esphome/components/climate/climate.h"
 #include "esphome/core/component.h"
-#include "esphome/components/remote_transmitter/remote_transmitter.h"
+#include "esphome/components/climate/climate.h"
+#include "esphome/components/remote_base/remote_base.h"
 
-namespace esphome
-{
-  namespace daikin200
-  {
+namespace esphome {
+namespace daikin200 {
 
-    class Daikin200Climate : public climate::Climate, public Component
-    {
-    public:
-      void setup() override;
-      void control(const climate::ClimateCall &call) override;
-      climate::ClimateTraits traits() override;
-      void set_transmitter(remote_transmitter::RemoteTransmitterComponent *tx)
-      {
-        this->transmitter_ = tx;
-      };
+class Daikin200Climate : public climate::Climate, public Component {
+ public:
+  void setup() override;
+  void control(const climate::ClimateCall &call) override;
+  climate::ClimateTraits traits() override;
 
-    protected:
-      // sender
-      void build_frame();
-      void send_frame();
-      uint8_t frame[25]{0}; // your Daikin200 payload buffer
+  void set_transmitter(remote_base::RemoteTransmitterBase *tx) {
+    this->transmitter_ = tx;
+  }
 
-      // Receiver helpers
-      bool validate_frame_(const uint8_t *frame, size_t len);
-      void decode_mode_(const uint8_t *frame);
-      void decode_temperature_(const uint8_t *frame);
-      void decode_fan_(const uint8_t *frame);
-      void decode_swing_(const uint8_t *frame);
+ protected:
+  remote_base::RemoteTransmitterBase *transmitter_{nullptr};
 
-      uint8_t calculate_checksum_(const uint8_t *frame);
-      bool validate_checksum_(const uint8_t *frame);
-      void process_received_frame(const uint8_t *data, size_t len);
-      remote_transmitter::RemoteTransmitterComponent *transmitter_{nullptr};
-    };
-  } // namespace daikin200
-} // namespace esphome
+  // state only (NOT IR encoding)
+  climate::ClimateMode mode_{climate::CLIMATE_MODE_OFF};
+  optional<float> target_temperature_;
+  optional<climate::ClimateFanMode> fan_mode_;
+  optional<climate::ClimateSwingMode> swing_mode_;
+
+  void send_state_();
+};
+
+}  // namespace daikin200
+}  // namespace esphome
