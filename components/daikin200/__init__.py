73f3047b4cb20edf2477d655690439c19b1a6905
borisqbf <borisqbf@users.noreply.github.com>
2026-06-01 13:12:07 +1000

    2026 version

--- components/daikin200/__init__.py
+++ components/daikin200/__init__.py
@@ -1,14 +1,10 @@
-print("\n\n🔥 DAIKIN200 INIT LOADED - 0 🔥\n\n")
+print("\n\n🔥 DAIKIN200 INIT LOADED - 1 🔥\n\n")
+
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import climate
 
 CODEOWNERS = ["@borisqbf"]
-
-DEPENDENCIES = ["climate"]
-
-AUTO_LOAD = ["climate"]
-
 
 daikin200_ns = cg.esphome_ns.namespace("daikin200")
 
@@ -18,18 +14,15 @@ Daikin200Climate = daikin200_ns.class_(
     cg.Component,
 )
 
-CONFIG_SCHEMA = climate.CLIMATE_SCHEMA.extend(
-    {
-        cv.GenerateID(): cv.declare_id(Daikin200Climate),
-    }
-).extend(cv.COMPONENT_SCHEMA)
+CONFIG_SCHEMA = cv.Schema({
+    cv.GenerateID(): cv.declare_id(Daikin200Climate),
+}).extend(cv.COMPONENT_SCHEMA)
 
 
-async def to_code(config):
+def to_code(config):
     var = cg.new_Pvariable(config[cv.ID])
-    await cg.register_component(var, config)
-
-    # IMPORTANT: explicit platform binding (this is what you are missing)
-    await climate.register_climate(var, config)
+    cg.add(var)
+    yield cg.register_component(var, config)
+    yield climate.register_climate(var, config)
 
 IS_PLATFORM = True
