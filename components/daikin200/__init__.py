76bd35545a807bed6a8dbe4e85d4e2ebdc1c1a39
borisqbf <borisqbf@users.noreply.github.com>
2026-06-01 17:15:48 +1000

    fixed errors

--- components/daikin200/__init__.py
+++ components/daikin200/__init__.py
@@ -1,11 +1,7 @@
+print("\n\n🔥 DAIKIN200 INIT LOADED - 1🔥\n\n")
+
 import esphome.codegen as cg
-import esphome.config_validation as cv
 from esphome.components import climate
-
-print("\n\n🔥 DAIKIN200 INIT LOADED - OK 🔥\n\n")
-
-DEPENDENCIES = ["climate"]
-AUTO_LOAD = ["climate"]
 
 daikin200_ns = cg.esphome_ns.namespace("daikin200")
 
@@ -14,13 +10,3 @@ Daikin200Climate = daikin200_ns.class_(
     climate.Climate,
     cg.Component,
 )
-
-CONFIG_SCHEMA = climate.CLIMATE_SCHEMA.extend({
-    cv.GenerateID(): cv.declare_id(Daikin200Climate),
-}).extend(cv.COMPONENT_SCHEMA)
-
-
-def to_code(config):
-    var = cg.new_Pvariable(config[cv.ID])
-    yield cg.register_component(var, config)
-    yield climate.register_climate(var, config)
--- components/daikin200/climate.py
+++ components/daikin200/climate.py
@@ -1,17 +1,16 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import climate
-from esphome.const import CONF_ID, CONF_NAME
 
 from . import Daikin200Climate
 
-DEPENDENCIES = ["climate"]
-
 CONFIG_SCHEMA = cv.Schema({
     cv.GenerateID(): cv.declare_id(Daikin200Climate),
-}).extend(climate.CLIMATE_SCHEMA).extend(cv.COMPONENT_SCHEMA)
+}).extend(cv.COMPONENT_SCHEMA)
 
 def to_code(config):
-    var = cg.new_Pvariable(config[CONF_ID])
+    var = cg.new_Pvariable(config[cv.ID])
+    cg.add(var)
     yield cg.register_component(var, config)
     yield climate.register_climate(var, config)
+    
