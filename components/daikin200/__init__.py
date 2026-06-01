96883d488eba86c5217beec228caebac581e2e59
borisqbf <borisqbf@users.noreply.github.com>
2026-06-01 16:59:45 +1000

    fixed component inheritance

--- components/daikin200/__init__.py
+++ components/daikin200/__init__.py
@@ -1,8 +1,14 @@
-print("\n\n🔥 DAIKIN200 INIT LOADED - 0🔥\n\n")
+print("\n\n🔥 DAIKIN200 INIT LOADED - 1🔥\n\n")
 
 import esphome.codegen as cg
 import esphome.config_validation as cv
 
 daikin200_ns = cg.global_ns.namespace("daikin200")
 
-Daikin200Climate = daikin200_ns.class_("Daikin200Climate")
+daikin200_ns = cg.esphome_ns.namespace("daikin200")
+
+Daikin200Climate = daikin200_ns.class_(
+    "Daikin200Climate",
+    climate.Climate,
+    cg.Component,
+)
