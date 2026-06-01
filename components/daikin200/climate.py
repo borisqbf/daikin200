901e8c7abb2ff0d989a436a97227270a5edcc185
borisqbf <borisqbf@users.noreply.github.com>
2026-06-01 20:02:54 +1000

    fixing transmitter errors

--- components/daikin200/climate.py
+++ components/daikin200/climate.py
@@ -1,19 +1,21 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import climate
+from esphome.components import remote_base
 
-from esphome.const import CONF_ID, CONF_NAME
+from esphome.const import CONF_ID, CONF_NAME, CONF_REMOTE_TRANSMITTER
+
 
 from . import Daikin200Climate
 
 CONFIG_SCHEMA = climate._CLIMATE_SCHEMA.extend({
     cv.GenerateID(): cv.declare_id(Daikin200Climate),
+    cv.Required(CONF_REMOTE_TRANSMITTER): cv.use_id(remote_base.RemoteTransmitterComponent),
 }).extend(cv.COMPONENT_SCHEMA)
 
 def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
-
-    tx = yield cg.get_variable(config["remote_transmitter"])
+    tx = yield cg.get_variable(config[CONF_REMOTE_TRANSMITTER])
     cg.add(var.set_transmitter(tx))
 
     yield cg.register_component(var, config)
