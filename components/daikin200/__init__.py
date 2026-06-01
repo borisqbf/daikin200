5276ba1e24d13bd342ebb7d47e5f893fb13417e8
borisqbf <borisqbf@users.noreply.github.com>
2026-06-01 14:32:37 +1000

    fixed error

--- components/daikin200/__init__.py
+++ components/daikin200/__init__.py
@@ -1,9 +1,8 @@
-print("\n\n🔥 DAIKIN200 INIT LOADED - 1🔥\n\n")
+print("\n\n🔥 DAIKIN200 INIT LOADED - 0🔥\n\n")
+
 import esphome.codegen as cg
+import esphome.config_validation as cv
 
-daikin200_ns = cg.esphome_ns.namespace("daikin200")
+daikin200_ns = cg.global_ns.namespace("daikin200")
 
-Daikin200Climate = daikin200_ns.class_(
-    "Daikin200Climate",
-    None,
-)
+Daikin200Climate = daikin200_ns.class_("Daikin200Climate")
