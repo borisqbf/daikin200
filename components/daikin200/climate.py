8df862c54976b0bfadf182df5c899f618b569b92
borisqbf <borisqbf@users.noreply.github.com>
2026-06-01 19:44:12 +1000

    removed diag

--- components/daikin200/climate.py
+++ components/daikin200/climate.py
@@ -2,16 +2,12 @@ import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import climate
 
-print("CLIMATE ATTRS:")
-print([x for x in dir(climate) if "SCHEMA" in x])
-
 from esphome.const import CONF_ID, CONF_NAME
 
 from . import Daikin200Climate
 
-CONFIG_SCHEMA = cv.Schema({
+CONFIG_SCHEMA = climate._CLIMATE_SCHEMA.extend({
     cv.GenerateID(): cv.declare_id(Daikin200Climate),
-    cv.Optional(CONF_NAME): cv.string,
 }).extend(cv.COMPONENT_SCHEMA)
 
 
