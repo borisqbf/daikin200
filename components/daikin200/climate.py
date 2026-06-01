f70005dc70bd69ba18b85bacfb44ce75a5bfe421
borisqbf <borisqbf@users.noreply.github.com>
2026-06-01 19:03:02 +1000

    more attempts to fix

--- components/daikin200/climate.py
+++ components/daikin200/climate.py
@@ -1,14 +1,14 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import climate
-from esphome.const import CONF_ID, CONF_NAME
+from esphome.const import CONF_ID
 
 from . import Daikin200Climate
 
-CONFIG_SCHEMA = cv.Schema({
+CONFIG_SCHEMA = climate.CLIMATE_SCHEMA.extend({
     cv.GenerateID(): cv.declare_id(Daikin200Climate),
-    cv.Required(CONF_NAME): cv.string,
 }).extend(cv.COMPONENT_SCHEMA)
+
 
 def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
